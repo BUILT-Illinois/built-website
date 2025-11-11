fa14acabb9d81847c9139c66346acd2bb98dbb4b
scuruchima1 <scuruchima1@gmail.com>
2025-11-10 20:02:51 -0600

    updated eboard cards

--- src/pages/aboutPage.js
+++ src/pages/aboutPage.js
@@ -57,92 +57,92 @@ export default AboutPage;
 
 const data = [
     {
-        image: 'eboard-elissa.png',
+        image: "/eboard/melissa.png",
         title: "President",
-        name: "Elisa Carillo",
-        pronouns: "she/her",
-        email: "elisamc2@illinois.edu",
-        description: "Hi! I am Elisa Carrillo, a Senior majoring in Computer Science. I grew up in Brooklyn and spent summers interning in San Diego, Sunnyvale, and Los Angeles. Currently, I develop for the Theorielearn group and serve as an MEP Peer Mentor. B[U]ILT’s growth has been inspiring, and I am grateful for the opportunity to be a leader and contribute to its success. In my free time, I love to watch Reality T.V., spend time with my family, run/hike, and do arts and crafts."
-    },
-    {
-        image: 'eboard-melissa.png',
-        title: "Vice President",
         name: "Melissa Aninagyei-Bonsu",
         pronouns: "she/her",
         email: "mba4@illinois.edu",
-        description: "Hey I'm Melissa. I'm a Junior studying Computer Science and minoring in Statistics! I am a CS STAR - CS Research Ambassador, and an Undergraduate Research Assistant! My hobbies include running track, watching Tiktok, and doing my hair, makeup, and nails! Catch me in the office on the daily!"
+        description: "Hey, I’m Melissa, President of B[U]ILT! I spend most of my time bouncing between classes, running track, playing flag football, and figuring out what to do with my hair next. Leading B[U]ILT is one of my favorite parts of college! I get to meet amazing people, plan fun events, and help create a community where we can all thrive."
     },
     {
-        image: 'eboard-adrian.png',
+        image: "/eboard/bolden.jpg",
+        title: "Vice President",
+        name: "Bolden Jones",
+        pronouns: "he/him",
+        email: "boldenj2@illinois.edu",
+        description: "Hi, my name is Bolden! I am a senior studying Computer Science with a minor in Statistics, from Flossmoor, IL. As this year’s Vice President, I’m excited to help lead B[U]ILT in fostering community, supporting our members, and strengthening the overall impact of our organization. In my free time, I like going to the gym, playing volleyball, and playing electric guitar."
+    },
+    {
+        image: "/eboard/adrian.png",
         title: "Treasurer",
         name: "Adrian Lara",
         pronouns: "he/him",
         email: "adrian11@illinois.edu",
-        description: "Hello! My name is Adrian Lara, and I am a junior majoring in Computer Science with a minor in the Hoeft Technology & Management Program. I am a current CS 128 Course Assistant, so you might catch me during office hours. "
+        description: "I'm a Pokemon enthusiast and horror film fanatic. Feel free to reach out for horror film recommendations or ask about what I'm currently watching!"
     },
     {
-        image:'eboard-xochi.png',
+        image: "/eboard/eduardo.jpg",
         title: "Secretary",
-        name: "Xochitl Zapata",
-        pronouns: "she/her",
-        email: "xzapata2@illinois.edu",
-        description: "Hi my name is Xochi Zapata, I study Brain and Cognitive Science and minoring in Computer Science. I’m from Blue Island, IL.  Some of my interests are philosophy, music and art!"
+        name: "Eduardo Morales",
+        pronouns: "he/him",
+        email: "emora38@illinois.edu",
+        description: "Hi! My name is Eduardo Morales, but I like to go by Eddy, and I am the secretary for the Fall 2025 semester! I am a senior studying Mathematics and Computer Science, and a little about me is that I love origami, Pókemon, and am an avid Letterboxd reviewer. If you ever see me around the B[U]ILT office, don't hesitate to reach out!"
     },
     {
-        image: 'eboard-libby.png',
+        image: "/eboard/carolina.png",
         title: "Internal Director",
-        name: "Libby Codamon",
-        pronouns: "she/her",
-        email: "ecoda2@illinois.edu",
-        description: "Hi! My name is Libby Codamon, and I am a senior studying Computer Science with a minor in the Hoeft Technology & Management Program. I am from the southside of Chicago near Midway Airport and come from a big Honduran-Puerto Rican family – I’m one of five siblings! I love cats, cooking, and theme parks! I also recently picked up coloring and playing pickleball. "
-
-    },
-    {
-        image: 'eboard-adela.png',
-        title: "Social Director",
-        name: "Adela Bautista Martinez",
-        pronouns: "she/her",
-        email: "adelab2@illinois.edu",
-        description: "Hi!! My name is Adela, I’m a second year cs + phil and psychology major from Evanston, IL. I’m excited to continue to build and grow our built community. In my free time I like traveling, eating, and watching tv. Feel free to reach out for anything!"
-    },
-    {
-        image: 'eboard-alejandro.png',
-        title: "External Director",
-        name: "Alejandro Chavez",
-        pronouns: "he/him",
-        email: "achav8@illinois.edu",
-        description: "Hi, I’m Alejandro Chavez I’m a senior studying Computer Science + Economics and I’m from Chicago, IL. In my free time I enjoy going to the gym, watching sports, and spending time with friends and family." 
-    },
-    {
-        image: 'eboard-bolden.jpg',
-        title: "Infrastructure Director",
-        name: "Bolden Jones",
-        pronouns: "he/him",
-        email: "boldenj2@illinois.edu",
-        description: "Hi my name is Bolden! I am a third year CS major with a minor in statistics, from Flossmoor, IL. As this year's infrastructure director I am excited to lead technical projects advancing our members' technical skills. In my free time I like to go to the gym, play volleyball, and watch anime."
-    },
-    {
-        image: 'eboard-alexis.jpg',
-        title: "Outreach Director",
-        name: "Alexis Serrano",
-        pronouns: "he/him",
-        email: "aserr3@illinois.edu",
-        description: "Hi, my name is Alexis Serrano, and I’m a fourth year Undergraduate student studying Computer Science. I’m from Berwyn, Illinois and really enjoy playing Tennis (and pickleball), listening to music, and running. As part of B[U]ILT, I’m the Outreach Director, and I look forward to expanding our CS community and helping students explore CS@UIUC. "
-    },
-    {
-        image: 'eboard-simon.png',
-        title: "Graduate Affairs Director",
-        name: "Simon Kato",
-        pronouns: "he/him",
-        email: "sk106@illinois.edu",
-        description: "Hi, I am a third-year Ph.D. candidate in computer science studying decision-making under uncertainty. In my free time, I enjoy rock climbing, running, and cooking." 
-    },
-    {
-        image:'eboard-carolina.jpg',
-        title: "Marketing Director",
         name: "Carolina Rosas",
         pronouns: "she/her",
         email: "cr46@illinois.edu",
-        description: "Hey! My name is Carolina Rosas, I am a junior studying Computer Science with a passion for public speaking and comedy! On campus, I am President of the Illini Speech and Debate Team and Marketing Director for B[U]ILT. In my free time I enjoy arts and crafts, karaoke, and going on walks! "
+        description: "Hi all! I am a Senior studying Computer Science, getting ready to graduate in the spring. I am beyond excited to get to meet at the new members who want to be part of this amazing community. Please feel free to reach out about anything! Fun fact about me, I love public speaking and comedy! I am on the Hive Society Comedy Club here on campus!"
+
+    },
+    {
+        image: "/eboard/jasmin.png",
+        title: "Social Director",
+        name: "Jasmin Salgado",
+        pronouns: "she/her",
+        email: "jsalg20@illinois.edu",
+        description: "Hello my name is Jasmin! I am a senior studying computer science and minoring in informatics. I am from Romeoville, IL. I love watching Netflix, taking pictures, and spending time with my family and friends! I am this year’s Social Director and I am excited to bring together our community through some fun and exciting social events!!"
+    },
+    {
+        image: "/eboard/alondra.jpg",
+        title: "External Director",
+        name: "Alondra Gonzalez",
+        pronouns: "he/him",
+        email: "ag131@illinois.edu",
+        description: "Alondra is a Senior in Computer Science who transferred to UIUC in Fall 2024. She is committed to making tech more accessible and inclusive, and through her role as External Director of B[U]ILT, she works to empower minority students to thrive in the industry. She has previous internship experiences at Atlassian and Microsoft and will be working full time at Atlassian after graduation." 
+    },
+    {
+        image: "/eboard/steven.jpeg",
+        title: "Infrastructure Director",
+        name: "Steven Uruchima",
+        pronouns: "he/him",
+        email: "scu2@illinois.edu",
+        description: "Hey everyone! I'm a Junior studying Computer Science and this years Infrastructure Director! I enjoy working on tech projects and am always open to meeting other B[U]ILT members!"
+    },
+    {
+        image: "/eboard/bijou.jpg",
+        title: "Outreach Director",
+        name: "Bijou Leinbach",
+        pronouns: "she/her",
+        email: " bijoucl2@illinois.edu",
+        description: "Hi! I’m Bijou, and I’m your outreach director this year! I’m a Junior this year and super excited to meet all of you:)"
+    },
+    {
+        image: "/eboard/dakarai.jpg",
+        title: "Graduate Affairs Director",
+        name: "Dakarai Crowder",
+        pronouns: "he/him",
+        email: "dcrowd3@illinois.edu",
+        description: "Hey I'm dakarai and I am a PhD student in computer science studying tactile sensing and human robot interaction. Some of my hobbies are baking, climbing, and thrifting." 
+    },
+    {
+        image: "/eboard/paloma.jpg",
+        title: "Marketing Director",
+        name: "Paloma Pichardo",
+        pronouns: "she/her",
+        email: "palomap3@illinois.edu",
+        description: "Hi! My name is Paloma Pichardo, and I am a sophomore studying Computer Science with a minor in Business. I am from Houston, Texas and really enjoy running, eating sushi, and the movie Fantastic Mr. Fox. I am so excited to serve as Marketing Director this school year and can’t wait to meet you all!"
     },
 ]
