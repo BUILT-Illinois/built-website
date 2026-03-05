a8aad9c41b452e53720fbe53c6094a5dfe98bc80
thaliag26 <73149504+thaliag26@users.noreply.github.com>
2026-03-05 14:48:18 -0600

    updated our new colorstack link for IGs on footer and Home page and corrected typo under the slack image Alt Text (#65)

--- src/components/welcomeCard.js
+++ src/components/welcomeCard.js
@@ -12,12 +12,12 @@ function WelcomeCard({name, imageSrc, description}) {
             </p>  
             <div className='icons'>
                 <a href='https://tinyurl.com/built-uiuc' target='_blank' rel='noopener noreferrer'>
-                    <img src = 'slack-svgrepo-com.svg' alt='twitter svg' />
+                    <img src = 'slack-svgrepo-com.svg' alt='slack svg' />
                 </a>
                 <a href='https://github.com/BUILT-Illinois' target='_blank' rel='noopener noreferrer'>
                     <img src = 'github-svgrepo-com.svg' alt='github svg' />
                 </a>
-                <a href='https://www.instagram.com/built_uiuc/' target='_blank' rel='noopener noreferrer'>
+                <a href='https://www.instagram.com/built_by_colorstack/' target='_blank' rel='noopener noreferrer'>
                     <img src = 'instagram-svgrepo-com.svg' alt='instagram svg' />
                 </a>
                 <a href='https://www.linkedin.com/in/built-uiuc/' target='_blank' rel='noopener noreferrer'>
